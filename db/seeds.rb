d290f9e46e3e09e2e67f93c2be627142728e6ec2
Constance Jiang <conzjiang@gmail.com>
2014-08-26 09:29:42 -0400

    Clean up Gemfile.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 Comment.create!(body: "Crackers are crunchy")
 Comment.create!(body: "I like Doritos because they are crispy")
 Comment.create!(body: "I wonder if the Taco Bell Quesarito is any good")
-Comment.create!(body: "My parents keep telling me I'm getting fat")
+Comment.create!(body: "I WANT TO GO TO THE BEACH")
